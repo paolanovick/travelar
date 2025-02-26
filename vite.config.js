4b4fc273801ed36ac8f29770d75266e30e09d39a
Paola Novick <paola.novick@davinci.edu.ar>
2025-02-25 21:29:32 -0300

    limpiando

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/travelar/", // Set the base path for GitHub Pages deployment
-    server: {
-      proxy: {
-        "/admin/xml": "https://travel-tool.net", // Proxy API calls during local development
-      },
-    },
+    
+     
+    
 });
