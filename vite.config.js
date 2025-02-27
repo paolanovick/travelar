f717274c11fb59444bb4756147f2fee1ac8163e5
Luis Gonzalez <lugo50635@gmail.com>
2025-02-27 15:12:18 -0300

    vercel ready

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()] 
+  plugins: [react()],
+  // Recuerda que esto lo debes sacar para vercel
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://travel-tool.net/admin/xml/allseasons.xml',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }   
 });
